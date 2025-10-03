a9c75af257706904af1c4028fe31ef62f0d34cf1
tomlangan <35125342+tomlangan@users.noreply.github.com>
2025-10-03 14:59:39 -0700

    Deployment updates

--- mongodb/init-mongo.sh
+++ mongodb/init-mongo.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -euo pipefail
+
 echo "init.sh: Initializing MongoDB..."
 
-MONGO_INITDB_ROOT_USERNAME=playday
-MONGO_INITDB_ROOT_PASSWORD=asdf1234
+MONGO_INITDB_ROOT_USERNAME=${MONGO_INITDB_ROOT_USERNAME:-playday}
+MONGO_INITDB_ROOT_PASSWORD=${MONGO_INITDB_ROOT_PASSWORD:-super-secret}
 
 echo "Checking if databases exist already..."
 
@@ -12,12 +14,8 @@ databases=("pd" "pd_acls")
 
 NEED_TO_RESTORE=false
 
-# Use username and password with admin auth
-mongosh --quiet --eval "db.getSiblingDB('admin').auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD')"
-
 for db in "${databases[@]}"; do
-    # Check if the database exists
-    db_exists=$(mongosh --quiet --username "$MONGO_INITDB_ROOT_USERNAME" --password "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase "admin" --eval "db.getMongo().getDBNames().includes('$db')")
+    db_exists=$(mongosh --quiet --username "$MONGO_INITDB_ROOT_USERNAME" --password "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase "admin" --eval "db.getMongo().getDBNames().includes('$db')" || echo "false")
 
     if [ "$db_exists" = true ]; then
         echo "Database $db already exists."
@@ -27,11 +25,9 @@ for db in "${databases[@]}"; do
     fi
 done
 
-# Restore the databases
-
 if [ "$NEED_TO_RESTORE" = true ]; then
     echo "Restoring databases..."
-    mongorestore  --username "$MONGO_INITDB_ROOT_USERNAME" --password "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase "admin" /dump
+    mongorestore --username "$MONGO_INITDB_ROOT_USERNAME" --password "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase "admin" /dump
 else
     echo "Databases already exist. Skipping restore."
 fi

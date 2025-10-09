3ae901ca6ef5ccf3ecbea3cfaba9bc934a27109a
tomlangan <35125342+tomlangan@users.noreply.github.com>
2025-10-08 23:54:47 -0700

    Update entrypoint.sh

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -7,6 +7,7 @@ echo "SERVER_NAME: ${SERVER_NAME}"
 echo "WS_LOCAL_PORT: ${WS_LOCAL_PORT}"
 echo "EXTERNAL_HTTP_PORT: ${EXTERNAL_HTTP_PORT}"
 echo "EXTERNAL_HTTPS_PORT: ${EXTERNAL_HTTPS_PORT}"
+echo "BASE_URL: ${BASE_URL}"
 
 PROTOCOL=$(echo "${PROTOCOL}" | tr '[:lower:]' '[:upper:]')
 
@@ -16,6 +17,6 @@ else
     cp /etc/nginx/nginx.conf.HTTP /etc/nginx/nginx.conf.template
 fi
 
-envsubst '${SERVER_NAME} ${WS_LOCAL_PORT} ${EXTERNAL_HTTP_PORT} ${EXTERNAL_HTTPS_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${SERVER_NAME} ${WS_LOCAL_PORT} ${EXTERNAL_HTTP_PORT} ${EXTERNAL_HTTPS_PORT} ${BASE_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
